2efe9fa295916fe6056c822ff4059b54230b7426
Vanderlei K Silva <vanderleis47@gmail.com>
2021-04-12 15:18:04 -0300

    create project

---
+++ hrworker/src/main/java/com/vanderleks/hrworker/HrworkerApplication.java
@@ -0,0 +1,13 @@
+package com.vanderleks.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrworkerApplication.class, args);
+	}
+
+}
