ed88ea23afa13674eb2ff07f9662d891bed6ba25
Vanderlei K Silva <vanderleis47@gmail.com>
2021-04-13 20:25:47 -0300

    implemented Histrix fault tolerance in hr-payroll

--- hrpayroll/src/main/java/com/vanderleiks/hrpayroll/controllers/PaymentController.java
+++ hrpayroll/src/main/java/com/vanderleiks/hrpayroll/controllers/PaymentController.java
@@ -1,14 +1,15 @@
 package com.vanderleiks.hrpayroll.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.vanderleiks.hrpayroll.entities.Payment;
+import com.vanderleiks.hrpayroll.services.PaymentService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.vanderleiks.hrpayroll.entities.Payment;
-import com.vanderleiks.hrpayroll.services.PaymentService;
 
 @RestController
 @RequestMapping("/payments")
@@ -17,8 +18,13 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
         return ResponseEntity.ok().body(paymentService.getPayment(workerId, days));
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        return ResponseEntity.ok().body(new Payment("", 0.0, days));
     }
 }
