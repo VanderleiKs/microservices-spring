0e526a6e69aae691a5c80375710840c67c233b96
Vanderlei K Silva <vanderleis47@gmail.com>
2021-04-13 20:33:11 -0300

    create project gateway zuul

---
+++ hrgatewayzuul/src/main/java/com/vanderleiks/hrgatewayzuul/HrgatewayzuulApplication.java
@@ -0,0 +1,13 @@
+package com.vanderleiks.hrgatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrgatewayzuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrgatewayzuulApplication.class, args);
+	}
+
+}
---
+++ hrgatewayzuul/src/test/java/com/vanderleiks/hrgatewayzuul/HrgatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanderleiks.hrgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrgatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
