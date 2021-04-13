058ed47f1a93314306f967487241153e15b58fef
Vanderlei K Silva <vanderleis47@gmail.com>
2021-04-13 18:50:06 -0300

    create project hreurekaserver

---
+++ hreurekaserver/src/test/java/com/vanderleiks/hreurekaserver/HreurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanderleiks.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HreurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
