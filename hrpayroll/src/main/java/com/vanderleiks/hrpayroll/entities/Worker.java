4c176a8c7dcdf0188a3f05410adef6005e2eefdc
Vanderlei K Silva <vanderleis47@gmail.com>
2021-04-13 16:23:52 -0300

    implemented Feign gives Spring Cloud at the project for conection between microservices

---
+++ hrpayroll/src/main/java/com/vanderleiks/hrpayroll/entities/Worker.java
@@ -0,0 +1,27 @@
+package com.vanderleiks.hrpayroll.entities;
+
+public class Worker{
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+    Worker(){}
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+}
--- hrpayroll/src/main/java/com/vanderleiks/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/com/vanderleiks/hrpayroll/services/PaymentService.java
@@ -1,57 +1,21 @@
 package com.vanderleiks.hrpayroll.services;
 
-import java.io.Console;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.print.event.PrintJobListener;
-
+import com.vanderleiks.hrpayroll.config.Feign;
 import com.vanderleiks.hrpayroll.entities.Payment;
+import com.vanderleiks.hrpayroll.entities.Worker;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
-    @Value("${worker.host}")
-    String workerHost;
-
-    @Autowired
-    private RestTemplate restTemplate;
+   @Autowired
+   private Feign feign;
 
     public Payment getPayment(Long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", workerId.toString());
-        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+        Worker worker = feign.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
 
-class Worker{
-    private Long id;
-    private String name;
-    private Double dailyIncome;
-    Worker(){}
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Double getDailyIncome() {
-        return dailyIncome;
-    }
-    public void setDailyIncome(Double dailyIncome) {
-        this.dailyIncome = dailyIncome;
-    }
-}
